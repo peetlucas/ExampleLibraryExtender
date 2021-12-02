5d02e5f4cf39d6de89f77a237df92e2c11d36f6e
Peter Shitote <pshitote@live.com>
2021-12-02 06:32:18 +0300

    Add project files.

---
+++ Exercise01/Exercise01.cs
@@ -0,0 +1,81 @@
+﻿using System.Numerics;
+
+
+namespace UtilityLibraries;
+
+public static class Exercise01
+{
+    public static string Towards(BigInteger number)
+    {
+        if (number == 0)
+            return "zero";
+
+        if (number < 0)
+           return "minus " + Towards(number);
+
+        string words = "";
+
+        if (number / 1000000000000000000 > 0)
+        {
+            words += Towards(number / 1000000000000000000) + " quintillion ";
+            number %= 1000000000000000000;
+        }
+        
+        if (number / 1000000000000000000 > 0)
+        {
+            words += Towards(number / 1000000000000000000) + " quadrillion ";
+            number %= 1000000000000000000;
+        }
+
+        if (number / 1000000000000000 > 0)
+        {
+            words += Towards(number / 1000000000000000) + " trillion ";
+            number %= 1000000000000000;
+        }
+
+        if (number / 1000000000000 > 0)
+        {
+            words += Towards(number / 1000000000000) + " billion ";
+            number %= 1000000000000;
+        }        
+
+        if ((number / 1000000) > 0)
+        {
+            words += Towards(number / 1000000) + " million ";
+            number %= 1000000;
+        }
+
+        if ((number / 1000) > 0)
+        {
+            words += Towards(number / 1000) + " thousand ";
+            number %= 1000;
+        }
+
+        if ((number / 100) > 0)
+        {
+            words += Towards(number / 100) + " hundred ";
+            number %= 100;
+        }
+
+        if (number > 0)
+        {
+            if (words != "")
+                words += "and ";
+
+            var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+            if (number < 20)
+                words += unitsMap[(int)number];
+            else
+            {
+                words += tensMap[(int)number / 10];
+                if ((number % 10) > 0)
+                    words += "-" + unitsMap[(int)number % 10];
+            }
+        }
+
+        return words;
+
+    }
+}
---
+++ Exercise02/Program.cs
@@ -0,0 +1,38 @@
+﻿using UtilityLibraries;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();
+
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input)) break;
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("The string description of the number is: " +
+                input.Towards());            
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+        return;
+
+        // Declare a ResetConsole local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"{Environment.NewLine}Press <Enter> only to exit; otherwise, enter a number and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
